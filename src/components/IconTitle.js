940964db9bb20175506148aa9e0e3792af2dbc7f
DylanVo28 <kedinhvo2000@gmail.com>
2022-07-09 00:24:31 +0700

    propsicon

--- src/components/IconTitle.js
+++ src/components/IconTitle.js
@@ -3,7 +3,8 @@ import  Typography  from '@mui/material/Typography';
 import  AllOutIcon  from '@mui/icons-material/AllOut';
 const IconTitle=(props)=>{
     return ( <Typography variant={props.variant} component={props.component} className={props.className}>
-    <span className='icon-title__icon'>{props.icon}</span><span className='icon-title__title'>{props.title}</span>
+    {props.icon&&<span className='icon-title__icon'>{props.icon}</span>}
+    <span className='icon-title__title'>{props.title}</span>
     </Typography>)
 }
 export default IconTitle
