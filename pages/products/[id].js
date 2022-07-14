5de5bf2f4cfc0f9da1a2789cf990286afb8edfd0
DylanVo28 <kedinhvo2000@gmail.com>
2022-07-14 15:43:47 +0700

    handle seo

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -170,6 +170,7 @@ var itemsBanner = [
     image:banner.src,
   },
 ];
+
 const ProductDetail = () => {
   const [width, setWidth] = useState(0);
   const [openModal, setOpenModal] = useState(false);
@@ -221,7 +222,7 @@ const ProductDetail = () => {
         asPath={router.asPath}
         keywords="keywords"
         ogTitle={`Lasted web development | ${APP_NAME}`}
-        ogDescription={'Tada description'}
+        ogDescription={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'}
         ogUrl={`${DOMAIN}${router.asPath}`}
         ogImage={banner.src}
 
